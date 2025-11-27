78033ca49d58bb59c0ab1d54bf1be5d6d4830609
Maxence SIBONI <maxencesibonibergard@gmail.com>
2025-11-27 17:27:52 +0100

    modification de terraform pour correctement utiliser cloud init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,93 +1,62 @@
 locals {
   pm_api_token_secret = trim(file("${path.module}/token-pve2.txt"), "\n")
 }
+
 resource "proxmox_vm_qemu" "proxmox" {
+  count       = var.instance_count
+  name        = element(var.name, count.index)
+  target_node = var.target_node
+  clone       = var.clone
+  full_clone  = true                     # Important: false = linked clone → Cloud-Init broken on most storage types
+  
+  cpu {
+    cores    = 1
+    sockets  = 1
+  }
+  memory   = 2048
 
-count = var.instance_count
+  # Network
+  network {
+    id = 0
+    model  = "virtio"
+    bridge = element(var.network_bridge, count.index)
+  }
 
-name = element(var.name,count.index)
-
-target_node = var.target_node
-
-clone = var.clone
-full_clone = false
-#cpu
-
-cpu {
-  cores = 1
-  sockets = 1
-}
-
-
-#memory
-
-memory = "2048"
-
-#network
-
-network {
-id = 0
-
-bridge = element(var.network_bridge,count.index)
-
-model = "virtio"
-
-}
-
-ipconfig0 = element(var.ip,count.index)
-
-nameserver = var.server_dns
-
-
+  # Disk
 disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.storage
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size     = var.size
+          storage  = var.storage
+          iothread = true
+        }
+      }
+    }
+  }
+  # Cloud-Init user data (this overwrites the template values)
+  os_type    = "cloud-init"
+  ciuser      = var.ciuser          # root
+  cipassword  = var.cipwd           # azertyuiop
+  ipconfig0   = element(var.ip, count.index)
+  nameserver  = var.server_dns
 
-scsi {
+  # Make the console work again in the web GUI
+  serial {
+    id   = 0
+    type = "socket"
+  }
+  vga {
+    type = "serial0"                # This is what makes noVNC show the real console
+  }
 
-scsi0 {
-
-disk {
-
-size = var.size
-
-storage = var.storage
-
-iothread = true
-
+  # Nice to have
+  agent = 1                         # qemu-guest-agent enabled
 }
-
-}
-
-}
-
-}
-
-#cloud init config
-
-os_type = "cloud-init"
-
-ciuser = var.ciuser
-
-cipassword = var.cipwd
-
-#sshkeys = <<EOF
-
-#${var.ssh_key}
-
-#EOF
-}
-# ----------------------------------------------------
-# Génération automatique du fichier Ansible inventory
-# ----------------------------------------------------
-resource "local_file" "ansible_inventory" {
-  depends_on = [proxmox_vm_qemu.proxmox]
-
-  content = <<EOT
-[all]
-%{ for index, vm in proxmox_vm_qemu.proxmox ~}
-${vm.name} ansible_host=${vm.ipconfig0} ansible_user=${var.ciuser} ansible_password=${var.cipwd}
-%{ endfor ~}
-EOT
-
-  filename = "${path.module}/../ansible/inventory.ini"
-}
-
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -2,19 +2,19 @@ pm_api_url = "https://172.19.3.19:8006/api2/json"
 
 pm_api_token_id = "root@pam!terraform"
 
-instance_count = 2
+instance_count = 1
 
-name = ["terraform-created-machine1","terraform-created-machine2"]
+name = ["terraform-created-machine1"]
 
-clone = "debian-13-desktop"
+clone = "debian-13-cloud-template"
 
 target_node = "pve2"
 
 #vmbr0 = LAN / vmbr1 = VM_Network / vmbr2 = DMZ
 
-network_bridge = ["vmbr0","vmbr0"]
+network_bridge = ["vmbr0"]
 
-ip = ["ip=172.19.30.100/16,gw=172.19.0.1","ip=172.19.30.101/16,gw=172.19.0.1"]
+ip = ["ip=172.19.30.100/16,gw=172.19.0.1"]
 
 server_dns = "192.168.1.1"
 
